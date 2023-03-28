ad0cd150c98dab9656baf186cd279d80b3da8a0f
Bogdan Markov <bogdick@yandex.ru>
2023-03-28 23:12:26 +0200

    TasksHeapBot: Main refactoring.

--- src/main/kotlin/bogdan/markov/tasksheapbot/Main.kt
+++ src/main/kotlin/bogdan/markov/tasksheapbot/Main.kt
@@ -4,7 +4,6 @@ import bogdan.markov.tasksheapbot.taskentity.TasksTable
 import bogdan.markov.tasksheapbot.tasksheapbot.TasksHeapBot
 import dev.inmo.tgbotapi.extensions.api.telegramBot
 import dev.inmo.tgbotapi.extensions.behaviour_builder.buildBehaviourWithLongPolling
-import kotlinx.coroutines.runBlocking
 import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.SchemaUtils
 import org.jetbrains.exposed.sql.transactions.transaction
@@ -12,20 +11,25 @@ import org.jetbrains.exposed.sql.transactions.transaction
 val CONFIG = loadConfig("config.yaml")
 
 suspend fun main() {
-    val bot = telegramBot(CONFIG.application.botApiToken)
-    val tasksHeapBot = TasksHeapBot()
+    databaseInit()
+    startBot()
+}
 
+private fun databaseInit() {
     Database.connect(
         "jdbc:h2:file:${CONFIG.database.path}",
         driver = "org.h2.Driver",
         user = CONFIG.database.user,
         password = CONFIG.database.password
     )
-    runBlocking {
-        transaction {
-            SchemaUtils.create(TasksTable)
-        }
+    transaction {
+        SchemaUtils.create(TasksTable)
     }
+}
+
+private suspend fun startBot() {
+    val bot = telegramBot(CONFIG.application.botApiToken)
+    val tasksHeapBot = TasksHeapBot()
 
     bot.buildBehaviourWithLongPolling {
         tasksHeapBot.runBot(this)
