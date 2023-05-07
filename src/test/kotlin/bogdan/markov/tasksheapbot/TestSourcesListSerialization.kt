91af403978f2e4de5841ea1b81f8300b95250b29
Bogdan Markov <bogdick@yandex.ru>
2023-05-07 19:21:10 +0200

    TasksHeapBot: try Jackson yaml - not working.

--- src/test/kotlin/bogdan/markov/tasksheapbot/TestSourcesListSerialization.kt
+++ src/test/kotlin/bogdan/markov/tasksheapbot/TestSourcesListSerialization.kt
@@ -1,6 +1,9 @@
 package bogdan.markov.tasksheapbot
 
 import com.charleskorn.kaml.Yaml
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory
+import com.fasterxml.jackson.module.kotlin.KotlinModule
 import dev.inmo.tgbotapi.types.message.textsources.*
 import dev.inmo.tgbotapi.utils.buildEntities
 import kotlinx.serialization.builtins.ListSerializer
@@ -8,6 +11,7 @@ import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 import org.junit.jupiter.api.Test
+import java.io.StringWriter
 import kotlin.test.assertEquals
 
 class TestSourcesListSerialization {
@@ -70,5 +74,34 @@ class TestSourcesListSerialization {
         println(serializedTextSources)
         val deserializedTextSource: TextSourcesList = Json.decodeFromString(serializedTextSources)
         assertEquals(textSources, deserializedTextSource)
+    }
+
+    @Test
+    fun testJsonSerialization2() {
+        // textSources=[RegularTextSource(source=Гав ), StrikethroughTextSource(source=Хрю, subsources=[RegularTextSource(source=Хрю)]), RegularTextSource(source= ), BoldTextSource(source=Мяу, subsources=[RegularTextSource(source=Мяу)])]
+        val textSources: TextSourcesList = buildEntities {
+            add(regular("Гав "))
+            add(strikethrough("Хрю"))
+            add(regular(" "))
+            add(bold("Мяу"))
+        }
+
+        println(textSources)
+
+        val mapper = ObjectMapper(YAMLFactory())
+        mapper.registerModule(KotlinModule.Builder().build())
+
+        val stringWriter = StringWriter()
+        mapper.writeValue(stringWriter, textSources)
+        val serializedTextSources = stringWriter.toString()
+        println(serializedTextSources)
+        // здесь получился кривой аутпут: не сериализуются type и value.
+
+        // с инпутом даже не стал заморачиваться.
+//        mapper.readValue<TextSourcesList>(StringReader(serializedTextSources), TextSourcesList::class.java)
+//        val deserializedTextSource: TextSourcesList = Json.decodeFromString(serializedTextSources)
+//        assertEquals(textSources, deserializedTextSource)
+
+
     }
 }
